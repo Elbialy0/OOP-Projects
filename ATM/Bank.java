39096e909556e7c348d2c992eb7865e41f707f9d
Elbialy0 <152164768+Elbialy0@users.noreply.github.com>
2024-02-15 10:11:48 +0200

    ATM

---
+++ ATM/Bank.java
@@ -0,0 +1,16 @@
+package ATM;
+
+import java.util.ArrayList;
+
+public class Bank {
+    // the name of the bank
+    private String name;
+    // users of this bank
+    private ArrayList<User> users;
+    // accounts in this bank
+    private ArrayList<Account> accounts;
+    // generate new UUID
+    public String getNewUUID(){
+
+    }
+}
---
+++ ATM/User.java
@@ -0,0 +1,47 @@
+package ATM;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.security.MessageDigest;
+
+public class User {
+    // First name of the user
+    private  String firstName;
+    // Second name of the user
+    private String lastName;
+    // Unique universal id
+    private String UUID;
+    // pin of the account
+    private byte[] pinHash;
+    // accounts that user have
+    private ArrayList<Account> accounts;
+
+    /**
+     * create a new user
+     * @param firstName the first name of the user
+     * @param lastName the last name of the user
+     * @param theBank the bank that the user is a customer of
+     * @param pin the password of user account
+     */
+
+    public User(String firstName, String lastName,Bank theBank,String pin){
+        // set the first name
+        this.firstName=firstName;
+        // set the second name
+        this.lastName=lastName;
+        // store pin's MD5 hash rather than the original value for security reasons
+        try {
+            MessageDigest md =MessageDigest.getInstance("MD5");
+            this.pinHash=md.digest(pin.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+           System.err.println("error , caught NoSuchAlgorithmException");
+        }
+        // create empty list of accounts
+        this.accounts = new ArrayList<>();
+        // git a new unique universal ID for the user
+        this.UUID = theBank.getNewUUID();
+
+
+    }
+
+}
